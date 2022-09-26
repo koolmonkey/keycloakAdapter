021eb491db7e4684b1f110ff8e6c64251f7d309c
Fedor Kuznetsov <dmesser@yandex.ru>
2022-09-26 11:53:14 +0500

    TODO done

--- keycloak-adapter/src/index.ts
+++ keycloak-adapter/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = 8080; // TODO: Добавить возможность получения через env
+const port = process.env["PORT"] || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
